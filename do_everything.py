239088019f4a06cbbab69d0c7f473115f6e2f38b
yglop <yglop@yandex.ru>
2024-04-07 23:38:54 +0500

    fixed visual buf, increase map size to 30x30

--- do_everything.py
+++ do_everything.py
@@ -7,24 +7,24 @@ from visuals.ui_main import UserInterfaceMain
 
 from map_gen import generate_map
 
-from dataset import tile_sprites_32, player_sprite, enemy_sprite
+from dataset import tile_sprites_64, tile_sprites_32, player_sprite, enemy_sprite
 
 class DoEvrything():
     def __init__(self):
         self.UI = UserInterfaceMain()
-        self.grid_size = 10
+        self.grid_size = 30
         self.group_tile = pg.sprite.RenderPlain()
         self.ent_visual_dict = dict()
         self.tile_map = dict() 
         self.create_tile_map()
-        self.create_ent()
+        self.create_ent_visual_dict()
         self.create_tiles()
 
     def create_tile_map(self):
         game_map = generate_map(
-            max_rooms=4,
+            max_rooms=12,
             room_min_size=3,
-            room_max_size=3,
+            room_max_size=12,
             map_width=self.grid_size, 
             map_height=self.grid_size
             )
@@ -35,7 +35,8 @@ class DoEvrything():
             2: tile_sprites_32[1],
             9: tile_sprites_32[1],
         }
-        tile_ent_preset = {
+
+        rile_ent_preset = {
             0: -1,
             1: 2,
             2: 100,
@@ -48,7 +49,7 @@ class DoEvrything():
                 pos_y = 32 + 32 * j
                            
                 tile_image = tile_image_preset[game_map[i,j]]
-                tile_ent = tile_ent_preset[game_map[i,j]]
+                tile_ent = rile_ent_preset[game_map[i,j]]
 
                 self.tile_map[(i,j)] = {
                     'image': tile_image,
@@ -56,15 +57,17 @@ class DoEvrything():
                     'rect.center': (pos_x, pos_y),
                     'entity': tile_ent,
                 }
-                
-    ##################################### THERE IS A BUG WITH VISUALS OF AN ENAMY
-    def create_ent(self):
+
+    def create_ent_visual_dict(self):
+        enemy_count = 1
         ## visuals
         for tile_id, tile_data in self.tile_map.items():
             if tile_data['entity'] == 2:
                 new_ent = EntityVisual(player_sprite, tile_data['rect'], tile_data['rect.center'])
                 self.ent_visual_dict[tile_data['entity']] = pg.sprite.RenderPlain(new_ent)
-            elif tile_data['entity'] not in (0, 2):
+            elif tile_data['entity'] not in (0, 2, -1):
+                enemy_count += 1
+                self.tile_map[tile_id]['entity'] += enemy_count
                 new_ent = EntityVisual(enemy_sprite, tile_data['rect'], tile_data['rect.center'])
                 self.ent_visual_dict[tile_data['entity']] = pg.sprite.RenderPlain(new_ent)
 
@@ -72,7 +75,7 @@ class DoEvrything():
         for tile_id, tile_data in self.tile_map.items():  
             tile = [tile_id, tile_data]  
             new_tile = TileDisplayer(tile)
-            print(tile) #debug
+
             self.group_tile.add(new_tile)
 
     def move_player(self, destination_tile, original_tile):
@@ -95,7 +98,7 @@ class DoEvrything():
         if self.tile_map[tile]['entity'] == 2:
             print('try_move_player:', tile ,"is player's tile")
         elif self.tile_map[tile]['entity'] == -1:
-            print('try_move_player:', tile ,"is impossuble")
+            print('try_move_player:', tile ,"is impossible")
         elif (tile[0] + 1 < self.grid_size and 
                 self.tile_map[(tile[0] + 1, tile[1])]['entity'] == 2):
             self.move_player(tile, (tile[0] + 1, tile[1]))
