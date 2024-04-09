710e36e20eb0faf6feb4920cf73ad13ca909efea
yglop <yglop@yandex.ru>
2024-04-09 16:39:44 +0500

    systems/map_system

--- do_everything.py
+++ do_everything.py
@@ -1,83 +1,22 @@
 import pygame as pg
 import random
 
-from visuals.display_tile import TileVisual
 from visuals.ui.main import UserInterfaceMain
-
-from mapgen.map_gen import generate_map
 
 from systems.entity_handler import EntityHandler
 from systems.keyboard_handler import keyboard_handler
 from systems.turn_system import TurnSystem
-
-from dataset import tile_sprites_32
+from systems.map_system import MapSystem
 
 class DoEvrything():
     def __init__(self):
         self.UI = UserInterfaceMain()
         self.TS = TurnSystem()
-        self.grid_size = 30
-        self.group_tile = pg.sprite.RenderPlain()
-        self.tile_map = dict() 
+        self.MS = MapSystem()
 
-        self.create_tile_map()
-        self.create_tiles()
-
-        self.ent_handler = EntityHandler(self.tile_map, self.grid_size, self.TS)
+        self.ent_handler = EntityHandler(self.MS.tile_map, self.MS.grid_size, self.TS)
         self.TS.get_max_player_actions(max_player_actions=2)
 
-    def create_tile_map(self):
-        game_map = generate_map(
-            max_rooms=12,
-            room_min_size=3,
-            room_max_size=12,
-            map_width=self.grid_size, 
-            map_height=self.grid_size
-            )
-
-        tile_image_preset = {
-            0: tile_sprites_32[1],
-            1: tile_sprites_32[2],
-            2: tile_sprites_32[2],
-            8: tile_sprites_32[3],
-            9: tile_sprites_32[2],
-        }
-
-        rile_ent_preset = {
-            0: -1,
-            1: 2,
-            2: 100,
-            8: 0,
-            9: 0,
-        }
-
-        for i in range(self.grid_size):
-            for j in range(self.grid_size):
-                pos_x = 32 + 32 * i
-                pos_y = 32 + 32 * j
-                           
-                tile_image = tile_image_preset[game_map[i,j]]
-                tile_ent = rile_ent_preset[game_map[i,j]]
-
-                self.tile_map[(i,j)] = {
-                    'image': tile_image,
-                    'rect': tile_image.get_rect(),
-                    'rect.center': (pos_x, pos_y),
-                    'entity': tile_ent,
-                }
-
-        enemy_count = 1
-        for tile_id, tile_data in self.tile_map.items():
-            if tile_data['entity'] >= 100:
-                enemy_count += 1
-                self.tile_map[tile_id]['entity'] += enemy_count
-
-    def create_tiles(self):
-        for tile_id, tile_data in self.tile_map.items():  
-            tile = [tile_id, tile_data]  
-            new_tile = TileVisual(tile)
-
-            self.group_tile.add(new_tile)
 
     def runner(self, event_list, screen):
         # UI
@@ -85,8 +24,8 @@ class DoEvrything():
         self.UI.render_turn_count(screen, self)
 
         # tiles
-        self.group_tile.update(event_list, self)
-        self.group_tile.draw(screen)
+        self.MS.group_tile.update(event_list, self)
+        self.MS.group_tile.draw(screen)
 
         ## tile ent's
         self.ent_handler.render_ents(screen)
---
+++ systems/map_system.py
@@ -0,0 +1,203 @@
+import pygame as pg
+
+from mapgen.map_gen import generate_map
+from visuals.display_tile import TileVisual
+from dataset import tile_sprites_32
+
+
+class MapSystem():
+    def __init__(self):
+        self.grid_size = 10
+        self.tile_map = dict() 
+        self.group_tile = pg.sprite.RenderPlain()
+
+        self.create_tile_map()
+        self.create_tiles()
+
+    def create_tile_map(self):
+        game_map = generate_map(
+            max_rooms=3,
+            room_min_size=3,
+            room_max_size=4,
+            map_width=self.grid_size, 
+            map_height=self.grid_size
+            )
+
+        tile_image_preset = {
+            0: tile_sprites_32[1],
+            1: tile_sprites_32[2],
+            2: tile_sprites_32[2],
+            8: tile_sprites_32[3],
+            9: tile_sprites_32[2],
+        }
+        rile_ent_preset = {
+            0: -1,
+            1: 2,
+            2: 100,
+            8: 0,
+            9: 0,
+        }
+
+        for i in range(self.grid_size):
+            for j in range(self.grid_size):
+                pos_x = 32 + 32 * i
+                pos_y = 32 + 32 * j
+                           
+                tile_image = tile_image_preset[game_map[i,j]]
+                tile_ent = rile_ent_preset[game_map[i,j]]
+
+                self.tile_map[(i,j)] = {
+                    'image': tile_image,
+                    'rect': tile_image.get_rect(),
+                    'rect.center': (pos_x, pos_y),
+                    'entity': tile_ent,
+                    'neighbors': self.set_neighbors((i,j))
+                }
+
+        enemy_count = 1
+        for tile_id, tile_data in self.tile_map.items():
+            if tile_data['entity'] >= 100:
+                enemy_count += 1
+                self.tile_map[tile_id]['entity'] += enemy_count
+
+    def create_tiles(self):
+        for tile_id, tile_data in self.tile_map.items():  
+            tile = [tile_id, tile_data]  
+            new_tile = TileVisual(tile)
+
+            self.group_tile.add(new_tile)
+
+    def set_neighbors(self, tile_id):
+        neighbors = list()
+        #   @@@@
+        #   @//@
+        #   @//@
+        #   @@@@
+        if (
+                (tile_id[0] > 0 and tile_id[0] < self.grid_size) and
+                (tile_id[1] > 0 and tile_id[1] < self.grid_size)
+            ):
+            neighbors.append((
+                (tile_id[0]+1, tile_id[1]+0), 
+                (tile_id[0]+0, tile_id[1]+1), 
+                (tile_id[0]-1, tile_id[1]+0), 
+                (tile_id[0]+0, tile_id[1]-1), 
+                (tile_id[0]+1, tile_id[1]+1), 
+                (tile_id[0]-1, tile_id[1]-1), 
+                (tile_id[0]+1, tile_id[1]-1), 
+                (tile_id[0]-1, tile_id[1]+1), 
+            ))
+        #   @//@
+        #   @@@@
+        #   @@@@
+        #   @@@@
+        elif (
+                (tile_id[0] == 0) and
+                (tile_id[1] > 0 and tile_id[1] < self.grid_size)
+            ):
+            neighbors.append((
+                (tile_id[0]+1, tile_id[1]+0), 
+                (tile_id[0]+0, tile_id[1]+1), 
+                (tile_id[0]+0, tile_id[1]-1),
+                (tile_id[0]+1, tile_id[1]+1),
+                (tile_id[0]+1, tile_id[1]-1),
+            ))
+        #   @@@@
+        #   @@@@
+        #   @@@@
+        #   @//@
+        elif (
+                (tile_id[0] == self.grid_size) and
+                (tile_id[1] > 0 and tile_id[1] < self.grid_size)
+            ):
+            neighbors.append((
+                (tile_id[0]-1, tile_id[1]+0), 
+                (tile_id[0]+0, tile_id[1]+1), 
+                (tile_id[0]+0, tile_id[1]-1),
+                (tile_id[0]-1, tile_id[1]-1),
+                (tile_id[0]-1, tile_id[1]+1),
+            ))
+        #   @@@@
+        #   @@@/
+        #   @@@/
+        #   @@@@
+        elif (
+                (tile_id[0] > 0 and tile_id[0] < self.grid_size) and
+                (tile_id[1] == self.grid_size)
+            ):
+            neighbors.append((
+                (tile_id[0]+1, tile_id[1]+0), 
+                (tile_id[0]-1, tile_id[1]+0), 
+                (tile_id[0]+0, tile_id[1]-1),
+                (tile_id[0]+1, tile_id[1]-1),
+                (tile_id[0]-1, tile_id[1]-1),
+            ))
+        #   @@@@
+        #   /@@@
+        #   /@@@
+        #   @@@@
+        elif (
+                (tile_id[0] > 0 and tile_id[0] < self.grid_size) and
+                (tile_id[1] == 0)
+            ):
+            neighbors.append((
+                (tile_id[0]+1, tile_id[1]+0), 
+                (tile_id[0]-1, tile_id[1]+0), 
+                (tile_id[0]+0, tile_id[1]+1),
+                (tile_id[0]+1, tile_id[1]+1),
+                (tile_id[0]-1, tile_id[1]+1),
+            ))
+        #   /@@@
+        #   @@@@
+        #   @@@@
+        #   @@@@
+        elif (
+                (tile_id[0] == 0) and
+                (tile_id[1] == 0)
+            ):
+            neighbors.append((
+                (tile_id[0]+1, tile_id[1]+0), 
+                (tile_id[0]+0, tile_id[1]+1), 
+                (tile_id[0]+1, tile_id[1]+1),
+            ))
+        #   @@@/
+        #   @@@@
+        #   @@@@
+        #   @@@@
+        elif (
+                (tile_id[0] == 0) and
+                (tile_id[1] == self.grid_size)
+            ):
+            neighbors.append((
+                (tile_id[0]+1, tile_id[1]+0), 
+                (tile_id[0]+0, tile_id[1]-1), 
+                (tile_id[0]+1, tile_id[1]-1),
+            ))
+        #   @@@@
+        #   @@@@
+        #   @@@@
+        #   /@@@
+        elif (
+                (tile_id[0] == self.grid_size) and
+                (tile_id[1] == 0)
+            ):
+            neighbors.append((
+                (tile_id[0]-1, tile_id[1]+0), 
+                (tile_id[0]+0, tile_id[1]+1), 
+                (tile_id[0]-1, tile_id[1]+1),
+            ))
+        #   @@@@
+        #   @@@@
+        #   @@@@
+        #   @@@/
+        elif (
+                (tile_id[0] == self.grid_size) and
+                (tile_id[1] == self.grid_size)
+            ):
+            neighbors.append((
+                (tile_id[0]-1, tile_id[1]-0), 
+                (tile_id[0]-0, tile_id[1]-1), 
+                (tile_id[0]-1, tile_id[1]-1),
+            ))
+            
+        return neighbors
