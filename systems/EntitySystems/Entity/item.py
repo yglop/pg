8822d9c937c34ba550f046612b8bd0fe8e5a3246
yglop <yglop@yandex.ru>
2024-04-10 17:32:42 +0500

    base items

--- systems/EntitySystems/Entity/item.py
+++ systems/EntitySystems/Entity/item.py
@@ -3,3 +3,33 @@
 class BaseItem():
     def __init__(self):
         self.space = 1
+
+
+class BaseWeaponRange(BaseItem):
+    def __init__(self):
+        self.name = 'BaseWeaponRange'
+
+
+class BaseWeaponMelee(BaseItem):
+    def __init__(self):
+        self.name = 'BaseWeaponMelee'
+
+
+class BaseLimb(BaseItem):
+    def __init__(self):
+        self.name = 'BaseLimb'
+
+
+class BaseOrgan(BaseItem):
+    def __init__(self):
+        self.name = 'BaseOrgan' 
+
+
+class BaseMutator(BaseItem):
+    def __init__(self):
+        self.name = 'BaseMutator' 
+
+
+class BaseBrainImplant(BaseItem):
+    def __init__(self):
+        self.name = 'BaseBrainImplant' 
