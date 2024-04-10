d6258d963ff0c41641b10afed61543ef4675553f
yglop <yglop@yandex.ru>
2024-04-10 16:56:56 +0500

    systems/EntitySystems/main

--- do_everything.py
+++ do_everything.py
@@ -3,7 +3,8 @@ import random
 
 from visuals.ui.main import UserInterfaceMain
 
-from systems.entity_system import EntitySystem
+from systems.EntitySystems.main import EntityManager
+
 from systems.keyboard_handler import keyboard_handler
 from systems.turn_system import TurnSystem
 from systems.map_system import MapSystem
@@ -14,8 +15,8 @@ class DoEvrything():
         self.TS = TurnSystem()
         self.MS = MapSystem()
 
-        self.ES = EntitySystem(self.MS.tile_map, self.MS.grid_size, self.TS)
-        self.TS.get_max_player_actions(self.ES.ent_stats_dict[2][0])
+        self.EM = EntityManager(self.MS, self.TS)
+        self.TS.get_max_player_actions(self.EM.ent_stats_dict[2][0])
 
 
     def runner(self, event_list, screen):
@@ -28,7 +29,7 @@ class DoEvrything():
         self.MS.group_tile.draw(screen)
 
         ## tile ent's
-        self.ES.render_ents(screen)
+        self.EM.render_ents(screen)
 
         # keyboard
         keyboard_handler(event_list, self)
