67c3ab38263f127a8f4d3d5383b3c3c9bb8268cc
yglop <yglop@yandex.ru>
2024-04-12 14:31:10 +0500

    TurnSystem naming inconsistency fix

--- systems/turn_system.py
+++ systems/turn_system.py
@@ -2,17 +2,17 @@ import pygame as pg
 
 
 class TurnSystem():
-    def __init__(self, ES):
-        self.ES = ES
+    def __init__(self, EM):
+        self.EM = EM
 
     def do_enemy_turn(self):
-        for ent_id, ent in self.ES.ent_stats_dict.items():
+        for ent_id, ent in self.EM.ent_stats_dict.items():
             if ent_id != 2:
-                self.ES.ES.try_move_enemy(ent_id)
+                self.EM.ES.try_move_enemy(ent_id)
                 ent.actions = ent.max_actions
 
     def end_turn(self):
         self.do_enemy_turn()
-        self.ES.ent_stats_dict[2].actions = self.ES.ent_stats_dict[2].max_actions
+        self.EM.ent_stats_dict[2].actions = self.EM.ent_stats_dict[2].max_actions
         print('End Turn')
         
