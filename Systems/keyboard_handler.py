8c0f6429353e8cee9a5dbe1b0377e1df2e682456
yglop <yglop@yandex.ru>
2024-04-14 22:47:05 +0500

    made keyboard_handler more readable

--- Systems/keyboard_handler.py
+++ Systems/keyboard_handler.py
@@ -8,18 +8,19 @@ def keyboard_handler(event_list, do_evrything):
                 print('keyboard_handler: quit')
                 pg.quit()   
 
-            if do_evrything.inventory_menu.is_menu_open == False:
-                if do_evrything.escape_menu.is_menu_open == False:
-                    if event.key == pg.K_SPACE: 
-                        do_evrything.TS.end_turn()
-                    elif event.key == pg.K_ESCAPE:
-                        do_evrything.escape_menu.open_menu()
-                else:
-                    if event.key == pg.K_ESCAPE:
-                        do_evrything.escape_menu.close_menu()
-            else:
+            if do_evrything.inventory_menu.is_menu_open == True:
                 if event.key == pg.K_ESCAPE:
                     do_evrything.inventory_menu.close_menu()
+                return
 
+            if do_evrything.escape_menu.is_menu_open == True: 
+                if event.key == pg.K_ESCAPE:
+                    do_evrything.escape_menu.close_menu()
+                return
+
+            if event.key == pg.K_SPACE: 
+                do_evrything.TS.end_turn()
+            elif event.key == pg.K_ESCAPE:
+                do_evrything.escape_menu.open_menu()
 
     
